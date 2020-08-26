ecf67c8c6151048b1121dc230bf2fad718c442fb
Tyler Wallace <tywallace2017@gmail.com>
2020-08-26 22:45:49 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,13 @@
+##def updated_array (colors_in_the_rainbow, next_color)
+  ## = ["One", "Two"]
+  ##using_push = ["Three", "four"]
+  ##updated_array.push("blue")
+  ##p updated_array
+##end
+
+def updated_array (colors_in_the_rainbow, next_color)
+  using_push = ["green", "orange"]
+  updated_array = using_push("yellow", "red")
+  updated_array.push ("blue")
+  p updated_array
+end
